86ab6d7c2b212404b7008682fb1704942c43a81b
NataleeBui <122491115+natalee-16@users.noreply.github.com>
2023-02-02 14:04:01 -0500

    added personal info

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -82,19 +82,18 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Natalee, but you can call me Nat");
         System.out.println();
 
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- To get a good job");
+        System.out.println("-- Have a good semester 2!");
         System.out.println();
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Reading");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Watching movies");
 
         System.out.println();
 
